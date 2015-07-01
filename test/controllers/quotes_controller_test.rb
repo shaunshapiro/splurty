c16ffc74f061032c606066f617aa8013d298864b
Shaun Shapiro <shaun.shapiro91@gmail.com>
2015-07-01 03:10:57 +0000

    added a show page to show specific quotes

--- test/controllers/quotes_controller_test.rb
+++ test/controllers/quotes_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class QuotesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "quote show page" do
+     quote = Quote.create(:author => 'Shaun Shapiro', :saying => 'Metal...GEAR?!?!')
+     get :show, :id => quote.id
+     assert_response :success
+   end
+
+   test "quote show page, not found" do
+   	get :show, :id => 'LOL'
+   	assert_response :not_found
+   end
 end
